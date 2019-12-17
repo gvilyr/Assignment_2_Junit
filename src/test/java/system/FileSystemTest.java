bbf289d91e9d5a548a75ba6f774dfcbc11dc21f0
Administrator <gvilyr@post.bgu.ac.il>
2019-12-17 18:57:50 +0200

    create java file and add junit

---
+++ src/test/java/system/FileSystemTest.java
@@ -0,0 +1,41 @@
+package system;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FileSystemTest {
+
+    @Test
+    public void dir() throws Exception{
+        FileSystem fileSystem=new FileSystem()
+    }
+
+    @Test
+    public void disk() {
+    }
+
+    @Test
+    public void file() {
+    }
+
+    @Test
+    public void lsdir() {
+    }
+
+    @Test
+    public void rmfile() {
+    }
+
+    @Test
+    public void rmdir() {
+    }
+
+    @Test
+    public void fileExists() {
+    }
+
+    @Test
+    public void dirExists() {
+    }
+}
